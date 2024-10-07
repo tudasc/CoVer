383d1eb87c6221c73116a10e51bacb0f9bca7663
N00byKing <N00byKing@hotmail.de>
2024-10-07 17:49:09 +0200

    Add branching to worklist algorithm

--- Passes/ContractVerifier.cpp
+++ Passes/ContractVerifier.cpp
@@ -1,11 +1,10 @@
 #include "ContractVerifier.hpp"
 #include "ContractManager.hpp"
 
-#include <algorithm>
-#include <iterator>
 #include <llvm/Analysis/InlineCost.h>
 #include <llvm/IR/BasicBlock.h>
 #include <llvm/IR/DebugInfoMetadata.h>
+#include <llvm/IR/Instruction.h>
 #include <llvm/IR/Instructions.h>
 #include <llvm/IR/IntrinsicInst.h>
 #include <llvm/IR/PassManager.h>
@@ -13,6 +12,8 @@
 #include <llvm/Support/Casting.h>
 #include <llvm/Transforms/Instrumentation.h>
 #include <set>
+#include <sstream>
+#include <utility>
 #include <vector>
 
 #include "../LangCode/ContractDataVisitor.hpp"
@@ -37,12 +38,14 @@ PreservedAnalyses ContractVerifierPass::run(Module &M,
                     continue;
                 }
                 if (isRead) {
-                    errs() << "Contract Fulfilled!\n";
+                    errs() << "## Contract Fulfilled!\n";
                     C.Status = ContractManagerAnalysis::FULFILLED;
                 } else {
-                    errs() << "Contract violation detected!\n";
+                    errs() << "##Contract violation detected!\n";
                     C.Status = ContractManagerAnalysis::BROKEN;
                 }
+                errs() << "## Function: " << C.F->getName() << "\n";
+                errs() << "## Contract: " << C.ContractString << "\n";
             }
         }
     }
@@ -57,30 +60,67 @@ PreservedAnalyses ContractVerifierPass::run(Module &M,
  */
 std::map<const Instruction*, std::set<ContractVerifierPass::RWStatus>> ContractVerifierPass::WorklistRW(Value* val, const Instruction* Start, const Function* F, bool must) {
     std::map<const Instruction*, std::set<RWStatus>> postAccess;
-    #warning HACK assume Instruction*
-    #warning TODO branches, "LUB"
-    const Instruction* next = Start->getNextNonDebugInstruction();
-    std::set<RWStatus> prevInfo;
-    while (next != nullptr) {
-        postAccess[next];
-        if (const LoadInst* Load = dyn_cast<LoadInst>(next)) {
-            if (Load->getPointerOperand() == val) {
-                // Variable is being loaded here. Add READ to this instruction
-                postAccess[next].insert(RWStatus::READ);
-            }
-        }
-        if (const StoreInst* Store = dyn_cast<StoreInst>(next)) {
-            if (Store->getPointerOperand() == val) {
-                // Variable is being stored here. Add WRITE to this instruction
-            postAccess[next].insert(RWStatus::WRITE);
-            }
-        }
-        // Add previous info
-        postAccess[next].insert(prevInfo.begin(), prevInfo.end());
+    std::vector<std::pair<const Instruction*, std::set<RWStatus>>> todoList = { {Start->getNextNonDebugInstruction(), std::set<RWStatus>() } };
+    while (!todoList.empty()) {
+        const Instruction* next = todoList.begin()->first;
+        std::set<RWStatus> prevInfo = todoList.begin()->second;
 
-        // Update for next iteration
-        prevInfo = postAccess[next];
-        next = next->getNextNonDebugInstruction();
+        while (next != nullptr) {
+            // Add previous info depending on following conditions:
+            // 1. In any case, prevInfo MUST have the corresponding access
+            // 2. Either: It is a "may" analysis, "next" was until now unreachable, or it was reached before and already contains the access
+            // If those apply, add that access. Otherwise, remove it if present (=> "must" analysis, node reached with info, jumped here without)
+            if (!postAccess.contains(next)) {
+                if (prevInfo.contains(RWStatus::READ)) postAccess[next].insert(RWStatus::READ);
+                if (prevInfo.contains(RWStatus::WRITE)) postAccess[next].insert(RWStatus::WRITE);
+            } else {
+                for (RWStatus s : { RWStatus::WRITE, RWStatus::READ } ) {
+                    if (prevInfo.contains(s) && (!must || postAccess[next].contains(s)))
+                        postAccess[next].insert(s);
+                    else
+                        postAccess[next].erase(s);
+                }
+            }
+
+            // Check for read or write operations
+            if (const LoadInst* Load = dyn_cast<LoadInst>(next)) {
+                if (Load->getPointerOperand() == val) {
+                    // Variable is being loaded here. Add READ to this instruction
+                    postAccess[next].insert(RWStatus::READ);
+                }
+            }
+            if (const StoreInst* Store = dyn_cast<StoreInst>(next)) {
+                if (Store->getPointerOperand() == val) {
+                    // Variable is being stored here. Add WRITE to this instruction
+                    postAccess[next].insert(RWStatus::WRITE);
+                }
+            }
+
+            // Check for branching / terminating instructions
+            // Missing because not sure if needed / relevant / used / too little info / lazy:
+            // CleanupReturnInst, CatchReturnInst, CatchSwitchInst, CallBrInst, ResumeInst, InvokeInst, IndirectBrInst
+            #warning TODO SwitchInst
+            if (const BranchInst* BR = dyn_cast<BranchInst>(next)) {
+                for (const BasicBlock* alt : BR->successors())
+                    todoList.push_back( {&alt->front(), postAccess[next]} );
+            }
+            if (isa<ReturnInst>(next) || isa<UnreachableInst>(next)) {
+                break;
+            }
+
+            if (getenv("LLVMCONTRACT_DEBUG") != NULL && atoi(getenv("LLVMCONTRACT_DEBUG")) == 1) {
+                std::stringstream out;
+                out << (postAccess[next].contains(RWStatus::READ) ? "R" : "") << (postAccess[next].contains(RWStatus::WRITE) ? "W" : "");
+                LLVMContext& C = next->getContext();
+                MDNode* N = MDNode::get(C, MDString::get(C, out.str()));
+                ((Instruction*)next)->setMetadata(("llvmcontract.RWdebug." + val->getName()).str(), N);
+            }
+
+            // Update for next iteration
+            prevInfo = postAccess[next];
+            next = next->getNextNonDebugInstruction();
+        }
+        todoList.erase(todoList.begin());
     }
     return postAccess;
 }
@@ -89,21 +129,24 @@ std::set<ContractVerifierPass::RWStatus> ContractVerifierPass::checkVarRW(std::s
     const BasicBlock& Entry = F->getEntryBlock();
     Value* varValue = nullptr;
     const DbgDeclareInst* decl = nullptr;
-    for (const Instruction& I : Entry) {
-        // Look for declaration of variable to get mapping of Variable -> Value
-        if (const DbgDeclareInst* DbgDec = dyn_cast<const DbgDeclareInst>(&I)) {
-            DILocalVariable* Variable = DbgDec->getVariable();
-            // Check variable name fits
-            if (Variable->getName() != var) continue;
+    for (const BasicBlock& B : *F) {
+        for (const Instruction& I : B) {
+            // Look for declaration of variable to get mapping of Variable -> Value
+            if (const DbgDeclareInst* DbgDec = dyn_cast<const DbgDeclareInst>(&I)) {
+                DILocalVariable* Variable = DbgDec->getVariable();
+                // Check variable name fits
+                if (Variable->getName() != var) continue;
 
-            // Check if scope fits
-            if (Variable->getScope() != F->getSubprogram()) continue;
+                // Check if scope fits
+                if (Variable->getScope() != F->getSubprogram()) continue;
 
-            errs() << "Found variable!" << "\n";
-            varValue = DbgDec->getVariableLocationOp(0);
-            decl = DbgDec;
-            break;
+                errs() << "Found variable!" << "\n";
+                varValue = DbgDec->getVariableLocationOp(0);
+                decl = DbgDec;
+                break;
+            }
         }
+        if (varValue != nullptr) break;
     }
 
     if (varValue == nullptr) {
@@ -112,7 +155,7 @@ std::set<ContractVerifierPass::RWStatus> ContractVerifierPass::checkVarRW(std::s
     }
 
     // For easier debugging
-    varValue->setName(var + ":" + F->getName());
+    varValue->setName(F->getName() + "." + var);
 
     std::map<const Instruction*, std::set<RWStatus>> AnalysisInfo = WorklistRW(varValue, decl, F, must);
 
