56fb180d8937a7fa7b19d4aefc7298c995e4bf04
N00byKing <N00byKing@hotmail.de>
2025-04-16 14:44:26 +0200

    Remove const

--- Passes/ContractVerifierRelease.hpp
+++ Passes/ContractVerifierRelease.hpp
@@ -16,7 +16,7 @@ class ContractVerifierReleasePass : public PassInfoMixin<ContractVerifierRelease
         static void appendDebugStr(std::vector<std::string>& err, const Instruction* Forbidden, const CallBase* CB);
     private:
         ReleaseStatus checkRelease(const ContractTree::ReleaseOperation relOp, ContractManagerAnalysis::LinearizedContract const& C, ContractExpression const& Expr, const Module& M, std::string& error);
-        std::map<const Function*, std::vector<TagUnit>> Tags;
+        std::map<Function*, std::vector<TagUnit>> Tags;
 };
 
 } // namespace llvm
