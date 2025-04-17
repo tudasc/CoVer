df6d042cc9a9fa5c81ed5970e02111afb2a2e7df
N00byKing <N00byKing@hotmail.de>
2025-04-17 15:20:34 +0200

    Test instrumentation

--- Include/DynamicAnalysis.h
+++ Include/DynamicAnalysis.h
@@ -8,12 +8,36 @@ struct Tag_t {
 };
 
 struct TagsMap_t {
-    void* functions; // List of function pointers
+    void** functions; // List of function pointers
     Tag_t* tags; // List of tags (matching above funcptr)
     int64_t count;
 };
 
+struct CallOp_t {
+    void** target_function;
+};
+struct CallTagOp_t {
+    const char* target_tag;
+};
+
+// For operations: Number must match those defined in ContractTree.hpp!
+enum ContractConnective : int64_t { UNARY_READ = 0, UNARY_WRITE = 1, UNARY_CALL = 2, UNARY_CALLTAG = 3, UNARY_RELEASE = 4, AND = 5, OR = 6, XOR = 7 };
+struct ContractFormula_t {
+    ContractFormula_t* children;
+    int64_t num_children;
+    ContractConnective conn;
+    void** data; // Only filled if conn == UNARY. Pointer to corresponding operation struct.
+};
+
+struct Contract_t {
+    ContractFormula_t precondition;
+    ContractFormula_t postcondition;
+    void* function;
+    const char* function_name;
+};
+
 struct ContractDB_t {
+    Contract_t* contracts;
+    int64_t num_contracts;
     TagsMap_t tags;
-    TagsMap_t tags1;
 };
--- Passes/Instrument.hpp
+++ Passes/Instrument.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "llvm/IR/PassManager.h"
+#include <llvm/IR/Constant.h>
 #include <llvm/IR/GlobalVariable.h>
 #include <memory>
 #include "ContractManager.hpp"
@@ -13,10 +14,18 @@ class InstrumentPass : public PassInfoMixin<InstrumentPass> {
         PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM);
 
     private:
-        std::pair<StructType*, Constant*> createTagGlobal(Module &M);
+        std::pair<StructType*, Constant*> createTagGlobal(Module &M); // Returns type, value
+        std::pair<Constant*, int64_t> createContractsGlobal(Module& M); // Returns value, number of elems (type is ptr)
+        Constant* createScopeGlobal(Module& M, std::vector<std::shared_ptr<ContractFormula>> forms); // Returns value, number of elems (type is ptr)
+        Constant* createFormulaGlobal(Module& M, std::shared_ptr<ContractFormula> form);
+        GlobalVariable* createConstantGlobalUnique(Module& M, Constant* C, std::string name);
         GlobalVariable* createConstantGlobal(Module& M, Constant* C, std::string name);
         PointerType* Ptr_Type; 
         IntegerType* Int_Type;
+        StructType* Formula_Type;
+        StructType* CallOp_Type;
+        StructType* CallTagOp_Type;
+        Constant* Null_Const;
         ContractManagerAnalysis::ContractDatabase* DB;
 };
 
