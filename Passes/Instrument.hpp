9e58aad0a728bb19291a5b2bcf39eeb7bc7cf8ad
N00byKing <N00byKing@hotmail.de>
2025-04-25 15:37:35 +0200

    Function instr testing

--- Passes/Instrument.hpp
+++ Passes/Instrument.hpp
@@ -3,7 +3,9 @@
 #include "llvm/IR/PassManager.h"
 #include <llvm/IR/Constant.h>
 #include <llvm/IR/GlobalVariable.h>
+#include <llvm/IR/InstrTypes.h>
 #include <memory>
+#include <set>
 #include "ContractManager.hpp"
 #include "ContractTree.hpp"
 
@@ -14,6 +16,7 @@ class InstrumentPass : public PassInfoMixin<InstrumentPass> {
         PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM);
 
     private:
+        // Structure Creation
         Constant* createTagGlobal(Module &M); // Returns type, value
         std::pair<Constant*, int64_t> createContractsGlobal(Module& M); // Returns value, number of elems (type is ptr)
         Constant* createScopeGlobal(Module& M, std::vector<std::shared_ptr<ContractFormula>> forms); // Returns value, number of elems (type is ptr)
@@ -21,8 +24,17 @@ class InstrumentPass : public PassInfoMixin<InstrumentPass> {
         GlobalVariable* createConstantGlobalUnique(Module& M, Constant* C, std::string name);
         GlobalVariable* createConstantGlobal(Module& M, Constant* C, std::string name);
         void createTypes(Module& M);
+
+        // Function Instrumentation
+        void instrumentFunctions(Module &M);
+        void insertFunctionInstrCallback(Function* CB);
+        FunctionCallee callbackFuncCallee;
+        std::set<Function*> already_instrumented;
+
+        // Types
         PointerType* Ptr_Type; 
         IntegerType* Int_Type;
+        Type* Void_Type;
         StructType* Formula_Type;
         StructType* DB_Type;
         StructType* Tag_Type;
@@ -31,6 +43,7 @@ class InstrumentPass : public PassInfoMixin<InstrumentPass> {
         StructType* CallTagOp_Type;
         StructType* Contract_Type;
         Constant* Null_Const;
+
         ContractManagerAnalysis::ContractDatabase* DB;
 };
 
