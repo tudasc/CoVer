c76ce511e2e73907ac9ef4a766ae536b624ee515
N00byKing <N00byKing@hotmail.de>
2025-07-24 15:24:04 +0200

    Add draft postcall analysis

---
+++ Dynamic/Analysis/Analyses/PostCallAnalysis.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "BaseAnalysis.h"
+#include "DynamicAnalysis.h"
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+struct PostCallAnalysis : BaseAnalysis {
+    public:
+        PostCallAnalysis(void* func_supplier, CallOp_t* callop);
+        PostCallAnalysis(void* func_supplier, CallTagOp_t* callop);
+        virtual Fulfillment onFunctionCall(void* location, void* func,  CallsiteParams params) override;
+        virtual Fulfillment onProgramExit(void* location) override;
+
+    private:
+        void SharedInit(void* _func_supplier, const char* target_str, CallParam_t *params, int64_t num_params);
+
+        // Configuration
+        void* func_supplier;
+        std::string target_str; // Either tag str or func str
+        std::vector<CallParam_t*> params; // Required parameters
+        std::set<void*> target_funcs;
+
+        // Analysis temporaries
+        std::map<void*, std::vector<CallsiteParams>> uncheckedCallsites;
+};
