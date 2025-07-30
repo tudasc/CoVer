210fe450064d2ce739b5d86da6319f17a1610311
N00byKing <N00byKing@hotmail.de>
2025-07-30 15:13:03 +0200

    Missing newline

--- Utils/ContractPassUtility.cpp
+++ Utils/ContractPassUtility.cpp
@@ -126,7 +126,7 @@ std::string getInstrLocStr(const Instruction* I) {
 
 FileReference getFileReference(const Instruction* I) {
     if (!I->getDebugLoc())
-        errs() << "Warning: Attempting to get file reference of instruction without debug information (most likely caused by outdated LLVM version)!\nThis may have adverse effects on report quality!";
+        errs() << "Warning: Attempting to get file reference of instruction without debug information (most likely caused by outdated LLVM version)!\nThis may have adverse effects on report quality!\n";
     return {
         .file = getFile(I),
         .line = I->getDebugLoc() ? I->getDebugLoc()->getLine() : 0,
