328d5557964fb58c77ac02b2c60e97e3a91f8986
N00byKing <N00byKing@hotmail.de>
2026-01-09 15:29:43 +0100

    Minor cleanup of tracemenu inputs

--- Utils/TUITrace.cpp
+++ Utils/TUITrace.cpp
@@ -1,19 +1,21 @@
 #include "TUITrace.hpp"
+#include <string>
+#include <vector>
 
 namespace TUITrace {
 
-std::string verifyInputArgs(std::string const& usage, std::string const& input, std::vector<int>& args, int const& num_inputs) {
-    std::istringstream iss(input);
+bool verifyInputArgs(std::string_view const& usage, std::string_view const& input, std::vector<std::string>& args, int const& num_inputs) {
+    std::istringstream iss(std::string(input), std::ios_base::in);
     // Throw away first input, which is the command itself
     iss.ignore(std::numeric_limits<std::streamsize>::max(), ' ');
     while (iss && args.size() <= num_inputs) {
-        int x;
+        std::string x;
         iss >> x;
         if (!iss) break;
         args.push_back(x);
     }
-    if (args.size() != num_inputs)  return "Invalid syntax for command. " + usage;
-    return "";
+    if (args.size() != num_inputs)  return false;
+    return true;
 }
 
 std::string traceKindToStr(ContractPassUtility::TraceKind kind) {
