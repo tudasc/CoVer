5a6edb6711006399606881a6a9420f3c2bfa73dd
N00byKing <N00byKing@hotmail.de>
2026-02-10 16:32:03 +0100

    Testing harness

---
+++ Tests/fort/PreCall-MissingInit.F90
@@ -0,0 +1,18 @@
+! RUN: %binaries/flangContracts --predefined-contracts --instrument-contracts %s -o %t.exe > %t.test_out 2>&1 && (%t.exe >> %t.test_out 2>&1 || true) && FileCheck %s < %t.test_out
+
+program PreCallMissingInit
+    use mpi_f08
+    implicit none
+    integer :: ierr
+
+    call MPI_Finalize(ierr)
+end program PreCallMissingInit
+
+! CHECK-LABEL: Running Contract Manager on Module
+! CHECK: Contract violation detected!
+! CHECK: CoVer: Total Tool Runtime
+
+! CHECK-LABEL: CoVer-Dynamic: Initializing...
+! CHECK: Contract violation detected!
+! CHECK: Missing Initialization call
+! Dont check for analysis finished, MPI implementation might crash.
