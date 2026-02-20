430be3ff875edcaf3d3db7f17c97cfe4df6c1069
N00byKing <N00byKing@hotmail.de>
2026-02-20 11:35:54 +0100

    Write coverage files to test output

--- Tests/fort/Correct-Minimal.F90
+++ Tests/fort/Correct-Minimal.F90
@@ -1,4 +1,4 @@
-! RUN: %binaries/flangContracts --predefined-contracts --instrument-contracts %s -o %t.exe > %t.test_out 2>&1 && %t.exe >> %t.test_out 2>&1 && FileCheck %s < %t.test_out
+! RUN: %binaries/flangContracts --predefined-contracts --instrument-contracts %s -o %t.exe > %t.test_out 2>&1 && COVER_COVERAGE_FOLDER="%t_coverage" %t.exe >> %t.test_out 2>&1 && FileCheck %s < %t.test_out
 
 program CorrectMinimal
     use mpi_f08
