224e4795803eba689d2280805456d8d76cbfe9e0
N00byKing <N00byKing@hotmail.de>
2026-03-11 12:59:06 +0100

    Add test for param

---
+++ Tests/fort/Param-InvalidComm.F90
@@ -0,0 +1,36 @@
+! RUN: %flangContracts %run_common
+
+program main
+    use mpi_f08
+    implicit none
+
+    integer :: rank
+    integer, pointer :: buf(:)
+    type(MPI_Request) :: req
+
+    call MPI_Init()
+
+    call MPI_Comm_rank(MPI_COMM_WORLD, rank)
+
+    allocate(buf(1))
+    buf(1) = 42
+    if (rank == 0) then
+        call MPI_Isend(buf, 1, MPI_INT, 1, 0, MPI_COMM_NULL, req)
+        print *, "Buf: ", buf(1)
+    else
+        call MPI_Irecv(buf, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, req)
+    end if
+    call MPI_Wait(req, MPI_STATUS_IGNORE)
+
+    call MPI_Finalize()
+end program
+
+! CHECK-LABEL: Running Contract Manager on Module
+! CHECK: Contract violation detected!
+! CHECK: Communicator is invalid
+! CHECK: CoVer: Total Tool Runtime
+
+! CHECK-LABEL: CoVer-Dynamic: Initializing...
+! CHECK: Contract violation detected!
+! CHECK: Communicator is invalid
+! Dont check if analysis finished, MPI implementation might crash.
