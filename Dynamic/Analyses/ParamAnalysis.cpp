6b46ff7ca1ca0d08186e0596bbd9ead253a267ed
N00byKing <N00byKing@hotmail.de>
2026-03-26 13:18:23 +0100

    Fix dynamic det of int param error comparisons

--- Dynamic/Analyses/ParamAnalysis.cpp
+++ Dynamic/Analyses/ParamAnalysis.cpp
@@ -15,27 +15,41 @@ Fulfillment ParamAnalysis::functionCBImpl(void* const& func, bool const isPre, C
         if (req->need_deref) {
             act_req = (const void*)DynamicUtils::TruncateBits(*(int64_t*)act_req, callsite.params[idx].size);
             act_callp = (const void*)DynamicUtils::TruncateBits(*(int64_t*)act_callp, callsite.params[idx].size);
+        } else {
+            act_callp = (const void*)DynamicUtils::TruncateBits((uintptr_t)act_callp, callsite.params[idx].size);
         }
         switch (req->comparator) {
-            case Comparator::NEQ:
-                if (act_callp != act_req) continue;
-                references.push_back(callsite.location); return Fulfillment::VIOLATED;
-            case Comparator::GTEQ:
-                if (act_callp >= act_req) continue;
-                references.push_back(callsite.location); return Fulfillment::VIOLATED;
-            case Comparator::GT:
-                if (act_callp >  act_req) continue;
-                references.push_back(callsite.location); return Fulfillment::VIOLATED;
-            case Comparator::LTEQ:
-                if (act_callp <= act_req) continue;
-                references.push_back(callsite.location); return Fulfillment::VIOLATED;
-            case Comparator::LT:
-                if (act_callp <  act_req) continue;
-                references.push_back(callsite.location); return Fulfillment::VIOLATED;
             case Comparator::EXEQ:
                 // EXEQ is the exception (pun), it overrides other forbidden values.
                 if (act_callp == act_req) return Fulfillment::FULFILLED;
                 continue;
+            case Comparator::NEQ:
+                if (act_callp != act_req) continue;
+                references.push_back(callsite.location); return Fulfillment::VIOLATED;
+            // Smaller/Larger comparisons dont make sense for pointers. Assume ints from here
+            default:
+                switch (callsite.params[idx].size) {
+                    case 8: act_callp = (void*)(int64_t)(int8_t)(uintptr_t)act_callp; break;
+                    case 16: act_callp = (void*)(int64_t)(int16_t)(uintptr_t)act_callp; break;
+                    case 32: act_callp = (void*)(int64_t)(int32_t)(uintptr_t)act_callp; break;
+                }
+                switch (req->comparator) {
+                    case Comparator::GTEQ:
+                        if ((int64_t)act_callp >= (int64_t)act_req) continue;
+                        references.push_back(callsite.location); return Fulfillment::VIOLATED;
+                    case Comparator::GT:
+                        if ((int64_t)act_callp >  (int64_t)act_req) continue;
+                        references.push_back(callsite.location); return Fulfillment::VIOLATED;
+                    case Comparator::LTEQ:
+                        if ((int64_t)act_callp <= (int64_t)act_req) continue;
+                        references.push_back(callsite.location); return Fulfillment::VIOLATED;
+                    case Comparator::LT:
+                        if ((int64_t)act_callp <  (int64_t)act_req) continue;
+                        references.push_back(callsite.location); return Fulfillment::VIOLATED;
+                    case Comparator::EXEQ:
+                    case Comparator::NEQ:
+                        __builtin_unreachable();
+                }
         }
     }
     return Fulfillment::UNKNOWN;
