3bafba6fdf4e84681a0150d5b8892984a142313f
N00byKing <N00byKing@hotmail.de>
2026-03-26 15:49:51 +0100

    Fix dynamic param again

--- Dynamic/Analyses/ParamAnalysis.cpp
+++ Dynamic/Analyses/ParamAnalysis.cpp
@@ -6,50 +6,50 @@
 #include <cstdint>
 #include <vector>
 
+static constexpr int64_t sign_extend(uintptr_t const ptr, int const size) {
+    switch (size) {
+        case 8: return (int64_t)(int8_t)ptr; break;
+        case 16: return (int64_t)(int16_t)ptr; break;
+        case 32: return (int64_t)(int32_t)ptr; break;
+    }
+    return ptr;
+}
+
 Fulfillment ParamAnalysis::functionCBImpl(void* const& func, bool const isPre, CallsiteInfo const& callsite) {
     if (func != func_supplier) return Fulfillment::UNKNOWN;
 
     for (const ParamReq_t* req : param_requirements) {
-        const void* act_req = req->isArg ? callsite.params[(int64_t)req->value].value: req->value;
-        const void* act_callp = callsite.params[idx].value;
+        void const* act_req = req->isArg ? callsite.params[(int64_t)req->value].value: req->value;
+        void const* act_callp = callsite.params[idx].value;
         if (req->need_deref) {
             act_req = (const void*)DynamicUtils::TruncateBits(*(int64_t*)act_req, callsite.params[idx].size);
             act_callp = (const void*)DynamicUtils::TruncateBits(*(int64_t*)act_callp, callsite.params[idx].size);
         } else {
             act_callp = (const void*)DynamicUtils::TruncateBits((uintptr_t)act_callp, callsite.params[idx].size);
         }
+        int64_t const int_req = (int64_t const)act_req;
+        int64_t const int_callp = sign_extend((uintptr_t)act_callp, callsite.params[idx].size);
         switch (req->comparator) {
             case Comparator::EXEQ:
                 // EXEQ is the exception (pun), it overrides other forbidden values.
-                if (act_callp == act_req) return Fulfillment::FULFILLED;
+                if (act_callp == act_req || int_callp == int_req) return Fulfillment::FULFILLED;
                 continue;
             case Comparator::NEQ:
-                if (act_callp != act_req) continue;
+                if (act_callp != act_req && int_callp != int_req) continue;
                 references.push_back(callsite.location); return Fulfillment::VIOLATED;
             // Smaller/Larger comparisons dont make sense for pointers. Assume ints from here
-            default:
-                switch (callsite.params[idx].size) {
-                    case 8: act_callp = (void*)(int64_t)(int8_t)(uintptr_t)act_callp; break;
-                    case 16: act_callp = (void*)(int64_t)(int16_t)(uintptr_t)act_callp; break;
-                    case 32: act_callp = (void*)(int64_t)(int32_t)(uintptr_t)act_callp; break;
-                }
-                switch (req->comparator) {
-                    case Comparator::GTEQ:
-                        if ((int64_t)act_callp >= (int64_t)act_req) continue;
-                        references.push_back(callsite.location); return Fulfillment::VIOLATED;
-                    case Comparator::GT:
-                        if ((int64_t)act_callp >  (int64_t)act_req) continue;
-                        references.push_back(callsite.location); return Fulfillment::VIOLATED;
-                    case Comparator::LTEQ:
-                        if ((int64_t)act_callp <= (int64_t)act_req) continue;
-                        references.push_back(callsite.location); return Fulfillment::VIOLATED;
-                    case Comparator::LT:
-                        if ((int64_t)act_callp <  (int64_t)act_req) continue;
-                        references.push_back(callsite.location); return Fulfillment::VIOLATED;
-                    case Comparator::EXEQ:
-                    case Comparator::NEQ:
-                        __builtin_unreachable();
-                }
+            case Comparator::GTEQ:
+                if (int_callp >= int_req) continue;
+                references.push_back(callsite.location); return Fulfillment::VIOLATED;
+            case Comparator::GT:
+                if (int_callp >  int_req) continue;
+                references.push_back(callsite.location); return Fulfillment::VIOLATED;
+            case Comparator::LTEQ:
+                if (int_callp <= int_req) continue;
+                references.push_back(callsite.location); return Fulfillment::VIOLATED;
+            case Comparator::LT:
+                if (int_callp <  int_req) continue;
+                references.push_back(callsite.location); return Fulfillment::VIOLATED;
         }
     }
     return Fulfillment::UNKNOWN;
